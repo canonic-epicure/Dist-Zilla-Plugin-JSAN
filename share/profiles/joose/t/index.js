8278e0227c71df7e1a15bba68dd2f3e611a1b17f
Nickolay Platonov <nickolay@desktop.(none)>
2010-11-22 13:43:25 +0300

    - updated profile

--- share/profiles/joose/t/index.js
+++ share/profiles/joose/t/index.js
@@ -19,7 +19,7 @@ Harness.configure({
         "jsan:Task.Joose.Core",
         "jsan:Task.JooseX.Namespace.Depended.Auto",
         {
-            text : "JooseX.Namespace.Depended.Manager.my.INC = " + Harness.prepareINC(INC)
+            text : "use.paths = " + Harness.prepareINC(INC)
         }
     ]
 })
